f4dbad7b8615b787b763188d93db5f5a7a7e0e9e
D2_49760_Devidas <devidaskaware96@gmail.com>
2021-10-16 15:08:47 +0530

    String_with_single_number_will_return_that_number_Functionality_completed

--- StringCalculator/src/main/java/calculator/StringCalculator.java
+++ StringCalculator/src/main/java/calculator/StringCalculator.java
@@ -2,9 +2,16 @@ package main.java.calculator;
 
 public class StringCalculator {
 
-	public int add(String string) {
-		// TODO Auto-generated method stub
+	public int add(String input) {
+		if(input.length()==1) {
+			return stringToIntegerConvertor(input);
+		}
 		return 0;
+	}
+
+	private int stringToIntegerConvertor(String input) {
+		
+		return Integer.parseInt(input);
 	}
 
 }
--- StringCalculator/src/test/java/calculator/StringCalculatorShould.java
+++ StringCalculator/src/test/java/calculator/StringCalculatorShould.java
@@ -7,9 +7,19 @@ import org.junit.jupiter.api.Test;
 import main.java.calculator.StringCalculator;
 
 public class StringCalculatorShould {
+
 	@Test
 	void emptyStringShouldReturn_0() {
 		StringCalculator stringCalculator = new StringCalculator();
+		// for empty string it will return 0
 		assertEquals(0, stringCalculator.add(""));
 	}
+
+	@Test
+	void stringWithSingleNumberShouldReturnNumberAsInt() {
+		StringCalculator stringCalculator = new StringCalculator();
+		//String with single number will return that number.
+		assertEquals(1, stringCalculator.add("1"));
+	}
+
 }
