ef4d307b6e99f302701e47afc99d54eccee55ee0
D2_49760_Devidas <devidaskaware96@gmail.com>
2021-10-16 15:02:17 +0530

    Empty_String_return_0_Functionality_completed

--- StringCalculator/src/main/java/calculator/StringCalculator.java
+++ StringCalculator/src/main/java/calculator/StringCalculator.java
@@ -2,4 +2,9 @@ package main.java.calculator;
 
 public class StringCalculator {
 
+	public int add(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- StringCalculator/src/test/java/calculator/StringCalculatorShould.java
+++ StringCalculator/src/test/java/calculator/StringCalculatorShould.java
@@ -1,5 +1,15 @@
 package test.java.calculator;
 
-public class StringCalculatorShould {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import main.java.calculator.StringCalculator;
+
+public class StringCalculatorShould {
+	@Test
+	void emptyStringShouldReturn_0() {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(0, stringCalculator.add(""));
+	}
 }
